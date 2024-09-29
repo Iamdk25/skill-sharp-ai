997a609d53b51146841dac87fd5934c4aa04c3f6
Iamdk25 <dkarmariya@usf.edu>
2024-09-29 00:29:24 -0400

    Header Completed

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -14,11 +14,10 @@ function Header() {
 
   return (
     <div className='flex p-4 items-center justify-between bg-secondary shadow-sm'>
-        <Image src={'/logo.svg'} width={160} height={100} alt='logo' />
+        <Image src={'app/public/logo_skillshare_ai.svg'} width={160} height={100} alt='logo' />
         <ul className='hidden md:flex gap-6'>
           <Link href={"/dashboard"}>
-            <li className={`hover:text-primary hover:font-bold transition-all
-            cursor-pointer
+            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer
             ${path=='/dashboard'&&'text-primary font-bold'}
             `}
             
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from './_components/Header';
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div>
+        <Header />
+    </div>
   )
 }
 
